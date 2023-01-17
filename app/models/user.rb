00cbacfe12276638bf26c5f7960a59c8dbe15e2b
adelguit <adeelguitoun@gmail.com>
2023-01-17 15:31:53 +0100

    Impliment the recent_three_post method for a given user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
+
+  def recent_three_posts
+    Post.where(user_id: self.id).order(updated_at: :desc).limit(3)
+  end
 end
