7e7a67f089f0c7d710d9bfed46e8095184796239
adelguit <adeelguitoun@gmail.com>
2023-01-17 15:52:27 +0100

    Implement update_posts_counter method for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  def update_posts_counter
+    User.find(self.user_id).increment!(:posts_counter)
+  end
 end
