b3e9207a5aff3e8db19af75eb08fc1ded3452cdb
adelguit <adeelguitoun@gmail.com>
2023-01-18 10:18:55 +0100

    Validate user model attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :name, presence: true
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
+
   def recent_three_posts
     Post.where(author_id: self.id).order(updated_at: :desc).limit(3)
   end
