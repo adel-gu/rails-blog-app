3bfb494672c743fdd02d5fde1bea6bc39dc5439f
adelguit <adeelguitoun@gmail.com>
2023-01-18 14:35:19 +0100

    Set default value for posts_counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,17 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
+  after_initialize :set_posts_counter_default
 
   validates :name, presence: true
-  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true, reater_than_or_equal_to: 0 }
 
   def recent_three_posts
-    Post.where(author_id: self.id).order(updated_at: :desc).limit(3)
+    Post.where(author: self).order(updated_at: :desc).limit(3)
+  end
+
+  private
+  def set_posts_counter_default
+    self.posts_counter = 0 unless self.posts_counter
   end
 end
