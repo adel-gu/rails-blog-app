83edd49f55a79f076785a785cb972d5b52b7d2c2
adelguit <adeelguitoun@gmail.com>
2023-01-18 16:09:00 +0100

    Add default values as 0 for likes_counter and comments_counter instead of nil, in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   after_save :update_posts_counter
+  after_initialize :set_comments_and_likes_counters
 
   validates :title, presence: true, length: { maximum: 250 }
 
@@ -13,5 +14,12 @@ class Post < ApplicationRecord
   # A method which returns the 5 most recent comments for a given post.
   def recent_five_comments
     Comment.where(post_id: self.id).order(updated_at: :desc).limit(5)
+  end
+
+  private
+
+  def set_comments_and_likes_counters
+    self.likes_counter = 0 unless self.likes_counter
+    self.comments_counter = 0 unless self.comments_counter
   end
 end
