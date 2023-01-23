e9c7fab1545f14c63980a28ea17fd2e18b651c18
adelguit <adeelguitoun@gmail.com>
2023-01-23 10:47:29 +0100

    Generate Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
   get 'users/index', to: 'users#index'
   get 'users/show', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
