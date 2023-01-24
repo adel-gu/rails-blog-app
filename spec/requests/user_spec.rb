3c28e94b3ac2d92ae07535eaa1ecc2b02b4f074b
adelguit <adeelguitoun@gmail.com>
2023-01-24 10:42:27 +0100

    Implement the testing for the request 'users/:id'

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -8,12 +8,29 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(:success)
     end
 
-    it 'to render the :index template' do
+    it 'renders the :index template' do
       expect(response).to render_template(:index)
     end
 
     it 'display header in the body response' do
       expect(response.body).to include("Here is a list of users")
+    end
+
+  end
+
+  describe 'GET /users/:id' do
+    before (:example) { get '/users/12' }
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the :show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'display header in the body response' do
+      expect(response.body).to include("Here is a user informations for a given user")
     end
 
   end
