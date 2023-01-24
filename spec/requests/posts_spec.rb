d6e644008d5a2e9a5b15aa2833999c681eae0922
adelguit <adeelguitoun@gmail.com>
2023-01-24 10:45:14 +0100

    Implement the testing for the request 'users/:id/posts'

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe 'GET /users/:id/posts' do
+    before (:example) { get '/users/12/posts' }
 
-  describe 'GET /show' do
     it 'returns http success' do
-      get '/posts/show'
       expect(response).to have_http_status(:success)
     end
+
+    it 'renders the :index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'display header in the body response' do
+      expect(response.body).to include("Here is a list of posts for a given user")
+    end
+
   end
 end
