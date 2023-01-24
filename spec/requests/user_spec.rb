81e14e2694a21d054b0789a7e4b62e90af2769ed
adelguit <adeelguitoun@gmail.com>
2023-01-24 10:39:38 +0100

    Implement the testing for '/users' request

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/user/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe 'GET /users' do
+    before (:example) { get '/users' }
 
-  describe 'GET /show' do
     it 'returns http success' do
-      get '/user/show'
       expect(response).to have_http_status(:success)
     end
+
+    it 'to render the :index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'display header in the body response' do
+      expect(response.body).to include("Here is a list of users")
+    end
+
   end
 end
