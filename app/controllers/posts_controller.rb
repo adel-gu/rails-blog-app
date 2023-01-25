a642d42656797591223edd32b214bd32b62e8800
adelguit <adeelguitoun@gmail.com>
2023-01-25 14:43:31 +0100

    Impelement the view of posts for a given user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author: @user)
+  end
 
   def show; end
 end
