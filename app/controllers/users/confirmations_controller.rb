f3f0a8e428089ce6474d8087ffc2f457faafc247
adelguit <adeelguitoun@gmail.com>
2023-02-01 11:29:30 +0100

    Remove unnecessary commented code

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,11 +1,2 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
-  # GET /resource/confirmation/new
-
-  # POST /resource/confirmation
-
-  # GET /resource/confirmation?confirmation_token=abcdef
-
-  # The path used after resending confirmation instructions.
-
-  # The path used after confirmation.
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,15 +1,4 @@
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-
-  # DELETE /resource/sign_out
-
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
