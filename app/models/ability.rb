96b51301e4701c32f09cfdf23ae4e10675a4ddb3
vflorin1995 <vflorin1995@gmail.com>
2023-02-02 10:00:35 +0200

    Add ability for author to delete a comment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,6 @@ class Ability
     can :manage, :all if user.role == 'admin'
 
     can :destroy, Post, author: user
-    # can :destroy, Comment, author: user
+    can :destroy, Comment, author: user
   end
 end
