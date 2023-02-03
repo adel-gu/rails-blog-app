6fb486e60a82827aefbd18b89771dc7045bcdf55
vflorin1995 <vflorin1995@gmail.com>
2023-02-03 10:10:27 +0200

    Add API controller for comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,27 +1,27 @@
-# module Api
-#   module V1
-#     class CommentsController < ApiController
-#       def index
-#         @post = Post.find(params[:post_id])
+module Api
+  module V1
+    class CommentsController < ApiController
+      def index
+        @post = Post.find(params[:post_id])
 
-#         render json: @post.comments
-#       end
+        render json: @post.comments
+      end
 
-#       def create
-#         @comment = Comment.new(comment_params)
+      def create
+        @comment = Comment.new(comment_params)
 
-#         if @comment.save
-#           render json: @comment, status: :created
-#         else
-#           render json: @comment.errors, status: :unprocessable_entity
-#         end
-#       end
+        if @comment.save
+          render json: @comment, status: :created
+        else
+          render json: @comment.errors, status: :unprocessable_entity
+        end
+      end
 
-#       private
+      private
 
-#       def comment_params
-#         params.require(:comment).permit(:author_id, :post_id, :text)
-#       end
-#     end
-#   end
-# end
+      def comment_params
+        params.require(:comment).permit(:author_id, :post_id, :text)
+      end
+    end
+  end
+end
